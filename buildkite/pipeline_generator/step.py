520567893e993051684ecb25a60a4ea496679e5f
khluu <khluu000@gmail.com>
2026-02-24 14:34:51 -0800

    Add test definition file as source file dependency itself
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/step.py
+++ buildkite/pipeline_generator/step.py
@@ -71,7 +71,7 @@ def read_steps_from_job_dir(job_dir: str):
                     ):
                         step.working_dir = "/vllm-workspace/tests"
                     step.source_file_dependencies = getattr(step, "source_file_dependencies", [])
-                    step.source_file_dependencies.append(yaml_path.split("/")[-3:-1])
+                    step.source_file_dependencies.append(os.path.relpath(yaml_path))
             steps.extend(file_steps)
     return steps
 
